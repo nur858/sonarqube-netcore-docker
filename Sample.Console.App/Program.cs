8d3353d747aa8249ccb400975a9f405c0bb7cffb
Nur Hasan <nur858@gmail.com>
2018-09-15 21:53:15 -0700

    initial commit

---
+++ Sample.Console.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.Console.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
